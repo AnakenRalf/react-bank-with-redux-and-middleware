25720b8c107b89dd267cb61e8e1e006f7705ca77
Anaken <anaken@gmail.com>
2023-11-09 23:44:25 +0300

    react bank application with redux and middleware

---
+++ src/features/customers/customerSlice.js
@@ -0,0 +1,76 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  fullName: "",
+  nationalId: "",
+  createdAt: "",
+};
+
+const customerSlice = createSlice({
+  name: "customer",
+  initialState,
+  reducers: {
+    createCustomer: {
+      prepare(fullName, nationalId) {
+        return {
+          payload: {
+            fullName,
+            nationalId,
+            createdAt: new Date().toISOString(),
+          },
+        };
+      },
+      reducer(state, action) {
+        state.fullName = action.payload.fullName;
+        state.nationalId = action.payload.nationalId;
+        state.createdAt = action.payload.createdAt;
+      },
+    },
+    update(state, action) {
+      state.fullName = action.payload;
+    },
+  },
+});
+
+export const { createCustomer, update } = customerSlice.actions;
+
+export default customerSlice.reducer;
+
+// const initialStateCustomer = {
+//   fullName: "",
+//   nationalId: "",
+//   createdAt: "",
+// };
+
+// export default function customerReducer(state = initialStateCustomer, action) {
+//   switch (action.type) {
+//     case "customer/createCustomer":
+//       return {
+//         ...state,
+//         fullName: action.payload.fullName,
+//         nationalId: action.payload.nationalId,
+//         createdAt: action.payload.createdAt,
+//       };
+//     case "customer/update":
+//       return {
+//         ...state,
+//         name: action.payload.name,
+//       };
+//     default:
+//       return state;
+//   }
+// }
+
+// export function createCustomer(fullName, nationalId) {
+//   return {
+//     type: "customer/createCustomer",
+//     payload: { fullName, nationalId, createdAt: new Date().toISOString() },
+//   };
+// }
+
+// export function updateCustomer(fullName) {
+//   return {
+//     type: "customer/update",
+//     payload: { fullName },
+//   };
+// }
